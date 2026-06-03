68460002c236f0ee356c8f0d380069dc3ed08f2c
namht <namht@NAMHT-PC>
2026-06-03 20:19:38 +0700

    Auto commit before deploy: 20:19:38 3/6/2026

--- server.js
+++ server.js
@@ -106,6 +106,7 @@ app.get('/api/events', (req, res) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Cache-Control', 'no-cache');
     res.setHeader('Connection', 'keep-alive');
+    res.setHeader('X-Accel-Buffering', 'no'); // Fix for NGINX buffering SSE
     res.flushHeaders();
 
     webClients.add(res);
